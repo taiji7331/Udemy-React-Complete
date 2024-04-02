a5964b3072f4560dc29dfb2c81e4c68580f09c0a
Kyle Seay <seay.kyle@systemi.co.jp>
2024-04-02 15:00:24 +0900

    first commit - adding files for section 3 up to lesson 48

---
+++ Section 3 - React Essentials/src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import { CORE_CONCEPTS } from "./data.js";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
