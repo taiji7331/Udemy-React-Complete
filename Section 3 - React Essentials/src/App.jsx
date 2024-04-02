618ccba3befe053329343a5952abe5152d12c83f
Kyle Seay <seay.kyle@systemi.co.jp>
2024-04-02 16:08:23 +0900

    adding changes up to lesson 51

--- Section 3 - React Essentials/src/App.jsx
+++ Section 3 - React Essentials/src/App.jsx
@@ -1,8 +1,13 @@
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import TabButton from "./components/Examples/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 
 function App() {
+  function handleSelect(selectedButton) {
+    console.log(`Hello, this is the ${selectedButton} button`);
+  }
+
   return (
     <div>
       <Header />
@@ -15,6 +20,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("Components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
---
+++ Section 3 - React Essentials/src/components/Examples/TabButton.jsx
@@ -0,0 +1,4 @@
+export default function TabButton({ children, onSelect }) {
+
+    return <li><button onClick={onSelect}>{children}</button></li>;
+}
